ea88f53ebff0300c9c23aa646f35bc9f90556c26
vanderweiss <acertig04@gmail.com>
2024-02-06 17:12:12 -0500

    fish

---
+++ generic/.config/fish/config.fish
@@ -0,0 +1,32 @@
+set -gx GOPATH '/vacht/.go'
+
+alias ls='exa -aln --octal-permissions --no-permissions --no-time'
+
+function fish_title
+    set -q argv[1]; or set argv fish
+    echo "$USER at $hostname": (fish_prompt_pwd_full_dirs=5 prompt_pwd);
+end
+
+function fish_greeting
+    echo (set_color -o blue)$USER (set_color white)'at' (set_color -o blue)$hostname
+end
+
+function fish_prompt
+    echo -n (set_color blue)(prompt_pwd) (set_color white)'❯'(set_color yellow)'❯'(set_color green)'❯ '  
+end
+
+function fish_right_prompt
+    set -l _status $status
+    if test $_status -ne 0
+        echo (set_color -o red)"E$_status"(set_color normal)
+    end
+end
+
+if status is-interactive
+end
+
+if status is-login
+    bass source /etc/profile
+end
+
+
