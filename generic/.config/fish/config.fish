c5b935a55929d271331761504c018341a68ed159
vanderweiss <acertig04@gmail.com>
2024-08-31 20:35:15 -0500

    fish setup, foot server and meiya procedural colorschemes

--- generic/.config/fish/config.fish
+++ generic/.config/fish/config.fish
@@ -2,11 +2,6 @@ alias l='eza -loa --no-permissions --smart-group --time-style=relative --git'
 alias c='clear'
 alias ff='fastfetch'
 
-function hx
-    command hx $argv
-    printf '\033[0 q'
-end
-
 function fish_greeting
 end
 
@@ -20,9 +15,6 @@ function fish_prompt
     fish_write normal "\n "
     fish_write magenta (prompt_pwd --full-length-dirs=99999)
     fish_write normal "\n := "
-end
-
-if status is-interactive
 end
 
 if status is-login
---
+++ generic/.config/fish/functions/setup.fish
@@ -0,0 +1,21 @@
+function setup
+    switch $argv[1]
+        case desktop
+            set -Ux CLUTTER_BACKEND wayland
+            set -Ux GDK_BACKEND wayland
+            set -Ux GTK_SCALE 1
+            set -Ux GTK_THEME Adwaita:dark
+            set -Ux QT_AUTO_SCREEN_SCALE_FACTOR 1
+            set -Ux QT_QPA_PLATFORM wayland
+            set -Ux QT_WAYLAND_DISABLE_WINDOWDECORATION 1
+            set -Ux QT_WPA_PLATFOMRTHEME qt5ct
+            set -Ux SDL_VIDEODRIVER wayland
+            set -Ux XDG_CURRENT_DESKTOP river
+            set -Ux XDG_SESSION_DESKTOP river
+            set -Ux XDG_SESSION_TYPE wayland
+            set -Ux TERM footclient
+            echo "Desktop environment setup done!"
+        case '*'
+            echo hi
+    end
+end
