84339c76aea43f04f917eefec3443a8e5ccf9a16
vanderweiss <acertig04@gmail.com>
2025-05-02 12:14:22 -0500

    chore: vars carryover

--- profiles/generic/.config/fish/functions/setup.fish
+++ profiles/generic/.config/fish/functions/setup.fish
@@ -13,8 +13,8 @@ function setup
             set -Ux QT_WAYLAND_DISABLE_WINDOWDECORATION 1
             set -Ux QT_WPA_PLATFOMRTHEME qt5ct
             set -Ux SDL_VIDEODRIVER wayland
-            set -Ux XDG_CURRENT_DESKTOP river
-            set -Ux XDG_SESSION_DESKTOP river
+            set -Ux XDG_CURRENT_DESKTOP niri
+            set -Ux XDG_SESSION_DESKTOP niri
             set -Ux XDG_SESSION_TYPE wayland
             set -Ux TERM footclient
             echo 'Desktop environment setup done!'
